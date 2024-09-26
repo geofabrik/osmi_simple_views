d2a825d3114bd8cce7b79c518a1566c7e8722dc4
Marc-marc-marc <31963329+Marc-marc-marc@users.noreply.github.com>
2024-09-26 09:31:34 +0200

    add was lifecycle prefix

--- src/tagging_view_handler.cpp
+++ src/tagging_view_handler.cpp
@@ -493,7 +493,7 @@ bool TaggingViewHandler::has_feature_key(const osmium::TagList& tags) {
             const auto keys = { "historic", "razed", "demolished",
                                 "abandoned", "disused", "construction",
                                 "proposed", "temporary", "TMC",
-                                "removed",
+                                "removed", "was",
                               };
             for (auto &&k : keys) {
                 if (is_a_x_key_key(t.key(), k)) {
